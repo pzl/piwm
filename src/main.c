bbd7c54d709ebaa0a542c34519bf9884b25650c1
pzl <dan@panzarel.la>
2016-01-26 13:32:38 -0500

    split off handling each client connection

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "gfx.h"
 #include "net.h"
 
+void handle_connection(int client_socket, struct sockaddr_storage remote_addr, Display screen);
 
 
 int main (int argc, char **argv) {
@@ -16,11 +17,8 @@ int main (int argc, char **argv) {
 	int client;
 
 	//GRAPHICS VARS
-	uint32_t img_data[32];
 	Display screen;
 
-
-	memset(img_data, 0, 32*sizeof(uint32_t));
 	screen = setup_graphics();
 	sock = setup_socket();
 
@@ -32,57 +30,11 @@ int main (int argc, char **argv) {
 	socklen_t addr_size = sizeof(remote_addr);
 
 
-	// graphics update vars
-	ClientWindow gfx;
-
 	while ( (client = accept(sock, (struct sockaddr *)&remote_addr, &addr_size)) > 0){
-
-		printf("got a connection\n");
-		get_client_name(&remote_addr);
-
-		printf("creating element\n");
-		gfx = create_window(screen);
-
-
-
-		char buf[2048];
-		ssize_t n_recvd;
-
-		while ( (n_recvd = recv(client, buf, 2048, 0)) > 0) {
-			/*
-			printf("< ");
-			for (int i=0; i<n_recvd; i++){
-				printf("[%d] ",buf[i]);
-			}
-			printf(">\n");
-			*/
-
-			if (n_recvd < 12){
-				printf("byte underflow. Only received %d\n", n_recvd);
-				continue;
-			}
-
-			if (n_recvd > 12){
-				printf("byte overflow. Got %d, wanted 12\n", n_recvd);
-			}
-
-
-			// sent as RGB, but bitmap is BGR
-			img_data[0] = buf[2] | buf[1] << 8 | buf[0] << 16;
-			img_data[1] = buf[5] | buf[4] << 8 | buf[3] << 16;
-			img_data[16]= buf[8] | buf[7] << 8 | buf[6] << 16;
-			img_data[17]= buf[11] | buf[10] << 8 | buf[9] << 16;
-			
-			window_update_graphics(&gfx, img_data);
-		}
-
-		//graphics cleanup
-		destroy_window(&gfx);
-		//network cleanup
-		close(client);
-
+		handle_connection(client, remote_addr, screen);
 		//@todo: method to break here, or catch signals
 		//and end up in code below, closing socket and cleaning up GPU
+		//would need to close threads and clients
 	}
 
 	close(sock);
@@ -90,4 +42,51 @@ int main (int argc, char **argv) {
 
 
 	return 0;
+}
+
+
+void handle_connection(int client, struct sockaddr_storage addr, Display screen) {
+	char buf[2048];
+	ssize_t n_recvd;
+	uint32_t img_data[32];
+	ClientWindow gfx;
+
+	printf("got a connection\n");
+	get_client_name(&addr);
+
+	printf("creating element\n");
+	gfx = create_window(screen);
+
+	memset(img_data, 0, 32*sizeof(uint32_t));
+	while ( (n_recvd = recv(client, buf, 2048, 0)) > 0) {
+		/*
+		printf("< ");
+		for (int i=0; i<n_recvd; i++){
+			printf("[%d] ",buf[i]);
+		}
+		printf(">\n");
+		*/
+
+		if (n_recvd < 12){
+			printf("byte underflow. Only received %d\n", n_recvd);
+			continue;
+		}
+
+		if (n_recvd > 12){
+			printf("byte overflow. Got %d, wanted 12\n", n_recvd);
+		}
+
+
+		// sent as RGB, but bitmap is BGR
+		img_data[0] = buf[2] | buf[1] << 8 | buf[0] << 16;
+		img_data[1] = buf[5] | buf[4] << 8 | buf[3] << 16;
+		img_data[16]= buf[8] | buf[7] << 8 | buf[6] << 16;
+		img_data[17]= buf[11] | buf[10] << 8 | buf[9] << 16;
+		
+		window_update_graphics(&gfx, img_data);
+	}
+
+	
+	destroy_window(&gfx); //graphics cleanup
+	close(client);        //network cleanup
 }
